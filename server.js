f9ef6a17061eb27b0202e797ff0c637f4ac374bd
andytompkins <andy.tompkins@gmail.com>
2016-01-06 09:50:54 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,118 @@
+var logManager = require('simple-node-logger');
+var exec = require('child_process').execSync;
+var express = require('express');
+var compression = require('compression');
+var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
+var passport = require('passport');
+var CustomStrategy = require('passport-custom').Strategy;
+var bodyParser = require('body-parser');
+var urlencode = require('urlencode');
+var json = require('json-middleware').middleware();
+//var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+
+var audit = logManager.createSimpleFileLogger('audit.log');
+var log = logManager.createSimpleLogger();
+
+var configFile = 'settings.json';
+var config = JSON.parse(fs.readFileSync(configFile));
+var home = config.home || '/home';
+
+var app = express();
+app.use(express.static(path.join(__dirname, '/assets')));
+app.use(compression());
+//app.use(cookieParser);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({ secret: 'chattymonkey', resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new CustomStrategy(function(req, done) {
+  log.debug("CustomStrategy");
+  var users = fs.readdirSync(home).filter(function(f) {
+    return fs.statSync(path.join(home, f)).isDirectory();
+  });
+  var u = _.find(users, function(user) {
+    return user === req.body.username;
+  });
+  if (!u) {
+    return done(null, false, { message: "Invalid username or password." });
+  }
+  if (!req.body.password) {
+    return done(null, false, { message: "Empty password." });
+  }
+  
+  var pub = u + '.pub';
+  var pkcs = u + '.pkcs';
+  var sig = u + '.sig';
+  
+  var decodeSig = 'base64 -d > ' + sig;
+  exec(decodeSig, { input: req.body.password });
+  
+  audit.info("Reading authorized keys of user " + u);
+  var getKeys = 'sudo su - ' + u + ' -c "cat ~/.ssh/authorized_keys"';
+  var stdout = exec(getKeys);
+  var lines = stdout.split('\n');
+  if (lines.length > 0) {
+    for (var i = 0; i < lines.length; i++) {
+      
+      fs.writeFileSync(pub, lines[i]);
+      var convert2Pkcs = 'ssh-keygen -e -f ' + pub + ' -m PKCS8 > ' + pkcs;
+      exec(convert2Pkcs);
+      var verifySig = 'openssl pkeyutl -verify -pubin -inkey ' + pkcs + ' -in unlock.txt -sigfile ' + sig;
+      stdout = exec(verifySig);
+      if (stdout === 'Signature Verified Successfully') {
+        return done(null, u);
+      }
+    }
+    return done(null, false, { message: "Invalid username or password." });
+  } else {
+    log.info('User ' + u + ' has no authorized keys.');
+    return done(null, false, { message: "Invalid username or password." });
+  }
+  
+  return;
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+var router = express.Router();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+
+router.get('/', function(req, res) {
+	res.render('index', {});
+});
+
+router.get('/login', function(req, res) {
+  console.log("login get");
+  res.render('login', {});
+});
+
+router.post('/login', passport.authenticate('custom', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}), function(req, res) {
+  console.log("login post");
+});
+
+
+app.use(router);
+
+var port = process.env.PORT || config.port || 3000;
+var server = app.listen(port, function() {
+  console.log("monkey listening on port " + port);
+});
+
+
+
